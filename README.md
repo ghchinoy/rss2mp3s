8dfe1622c9a48fe0afea3475c982458f59940e55
G. Hussain Chinoy <ghchinoy@gmail.com>
2021-08-07 12:52:37 -0600

    first

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 Have you ever wanted to download all the mp3s from a podcast rss feed? This is for you.
 
+Here's a [video](https://threadit.app/thread/plpazglp38ddilsidl5g/message/s4m04bhb52yavk4t9d68n14g)
+
+![rss2mp3s](samples/threadit-rss2mp3s.gif)
+
+## FAQ
+
+* Does it do a lot of checking? No
+* Can it do other things? Maybe, [file a feature request](https://github.com/ghchinoy/rss2mp3s/issues)
+
+
+
+## References
+
+* Apple Podcasts https://podcasters.apple.com/support/823-podcast-requirements
+* Google Podcasts https://support.google.com/podcast-publishers/answer/9889544?hl=en
+
 
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
+	"strings"
+	"sync"
 
 	"github.com/ghchinoy/rss2mp3s/readfeed"
 )
@@ -31,8 +35,39 @@ func main() {
 		os.Exit(1)
 	}
 	log.Printf("ok, rss (%d): '%s'", rss.Length, rss.Channel.Title)
+	var waitGroup sync.WaitGroup
+	log.Printf("Found %d items", len(rss.Channel.Items))
 	for _, items := range rss.Channel.Items {
-		log.Printf("%s", items.Title)
-		log.Printf("\t%v", items.Enclosure)
+		//log.Printf("beginning download of %s", items.Title)
+		waitGroup.Add(1)
+		go downloadEnclosure(items.Title, items.Enclosure.URL, &waitGroup)
 	}
+	waitGroup.Wait()
+}
+
+// downloadEnclosure downloads the target enclosure URL to a local file
+func downloadEnclosure(title, enclosureURL string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// create a name for the file from the URL
+	parts := strings.Split(enclosureURL, "/")
+	filename := parts[len(parts)-1:][0]
+	var writer io.WriteCloser
+	writer, err := os.Create(filename)
+	if err != nil {
+		log.Printf("unable to create output file '%s': %v", filename, err)
+		return
+	}
+	defer writer.Close()
+
+	r, err := http.Get(enclosureURL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	io.Copy(writer, r.Body)
+	if err = r.Body.Close(); err != nil {
+		fmt.Println(err)
+	}
+	log.Printf("Downloaded '%s' as %s", title, filename)
 }
